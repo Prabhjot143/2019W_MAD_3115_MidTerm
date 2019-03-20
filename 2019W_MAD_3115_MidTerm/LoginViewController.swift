6285bc970bbe5461fd696814a534f86831d6a3d1
MacStudent <macstudent@126-imac19.local>
2019-03-20 18:46:50 -0400

    commit

--- 2019W_MAD_3115_MidTerm/LoginViewController.swift
+++ 2019W_MAD_3115_MidTerm/LoginViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailtxt: UITextField!
+    
+    @IBOutlet weak var pwdtxt: UITextField!
+    
+    @IBOutlet weak var RememberMe: UISwitch!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     let userDefault = UserDefaults.standard
+      if let email = userDefault.string(forKey: "userEmail"){
+        emailtxt.text =  email
+         if let password = userDefault.string(forKey: "userPassword"){
+            pwdtxt.text =  password
+            
+        }
+        RememberMe.isOn = true
+        }
+        
     }
 
-
+    @IBAction func loginBtn(_ sender: Any) {
+        let email = emailtxt.text
+        let password = pwdtxt.text
+        if(email == "admin@123" && password == "admin")
+        {
+             let userDefault = UserDefaults.standard
+            if RememberMe.isOn
+            {
+                userDefault.set(email, forKey: "userEmail")
+                userDefault.set(password, forKey: "userPassword")
+            }
+            else
+            {
+                userDefault.removeObject(forKey: "userEmail")
+                 userDefault.removeObject(forKey: "userPassword")
+            }
+            let homeVC = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController(WithIdentifier : "StudentEntryViewController") as! StudentEntryViewController
+            self.present Home VC
+        }
+        
+    }
+    
 }
 
